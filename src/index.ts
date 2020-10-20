e0ea7e2ffe49199b76f4ba6d483e96e6d5af24d8
Greg Sithole <gregsithole@hotmail.co.za>
2020-10-21 01:36:36 +0200

    Changed format of Index

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ const program = require('commander');
 const search = require('./functions/search');
 
 program
-  .command('search')
-  .description('Search for a Pokémon')
-  .action(function () {
-    search();
-  });
+    .command('search')
+    .description('Search for a Pokémon')
+    .action(function () {
+        search();
+    });
 
 program.parse(process.argv);
