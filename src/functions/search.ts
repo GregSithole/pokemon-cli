9f7204cf9dc9987785a5fa075020856b2d80e6f7
Greg Sithole <gregsithole@hotmail.co.za>
2020-10-21 01:36:56 +0200

    Updated Search.ts to have initial search function

--- src/functions/search.ts
+++ src/functions/search.ts
@@ -1,3 +1,93 @@
-module.exports = () => {
-  console.log(`Searching for pokemon...`);
+import { AxiosError, AxiosResponse } from 'axios';
+
+const inquirer = require('inquirer');
+const ora = require('ora');
+const axios = require('axios');
+const Table = require('cli-table');
+const chalk = require('chalk');
+const _ = require('lodash');
+
+function runSearch(answers: any) {
+    const searchLoading = ora().start(`Beginning to search for Pokémon...`);
+    axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${answers.name.toLowerCase()}`)
+        .then(function (response: AxiosResponse) {
+            searchLoading.succeed(
+                `Successfully found Pokémon: ${answers.name}`
+            );
+            const result = displayPokemon(response.data);
+            console.log(result.toString());
+        })
+        .catch(function (error: AxiosError) {
+            searchLoading.fail(`We could not find the Pokémon`);
+            console.log(error);
+        });
 }
+
+function displayPokemon(result: any) {
+    const displayedTable = new Table();
+    displayedTable.push(
+        {
+            [`${chalk.green.bold('ID')}`]: result.id,
+        },
+        {
+            [`${chalk.green.bold('Name')}`]: _.upperFirst(result.name),
+        },
+        {
+            [`${chalk.green.bold('Type')}`]: _.join(
+                _.map(result.types, (type: any) => {
+                    return _.capitalize(type.type.name);
+                }),
+                ', '
+            ),
+        },
+        {
+            [`${chalk.green.bold('Abilities')}`]: _.join(
+                _.map(result.abilities, (abilities: any) => {
+                    return _.capitalize(abilities.ability.name);
+                }),
+                ', '
+            ),
+        },
+        {
+            [`${chalk.green.bold('Weight')}`]: result.weight / 10 + `kg`,
+        }
+    );
+
+    return displayedTable;
+}
+
+module.exports = () => {
+    const questions = [
+        {
+            type: 'list',
+            name: 'condition',
+            message: 'How would you like to search for Pokémon?',
+            choices: ['ID', 'Name', 'Generation'],
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter the Pokémon Name:',
+            when: (answers: any) => answers.condition === 'Name',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter the Pokémon ID:',
+            when: (answers: any) => answers.condition === 'ID',
+        },
+        {
+            type: 'input',
+            name: 'generation',
+            message: 'Please enter the Pokémon Generation:',
+            when: (answers: any) => answers.condition === 'Generation',
+        },
+    ];
+
+    const prompt = inquirer.createPromptModule();
+
+    prompt(questions).then((answers: any) => {
+        runSearch(answers);
+    });
+};
