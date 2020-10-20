b1c1796029b574da24714711238341f2b6128b55
Greg Sithole <gregsithole@hotmail.co.za>
2020-10-20 23:42:52 +0200

    Basic Setup complete

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const path = require('path');
+const program = require('commander');
